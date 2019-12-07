d8f88e8d750d6910e8ec19c6fe52ad7922ca1e68
mandred <mandred1991@gmail.com>
2019-12-07 13:48:29 +0300

    init

---
+++ src/layouts/LayoutMain.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'LayoutMain',
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/layouts/LayoutOverlay.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'LayoutOverlay',
+};
+</script>
+
+<style lang="scss">
+
+</style>
