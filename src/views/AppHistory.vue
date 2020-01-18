0202bb1417bcbdf62bc0df1db13a1bc1fd0bd764
mandred <mandred1991@gmail.com>
2020-01-19 00:17:22 +0300

    add random color diagram

--- src/views/AppHistory.vue
+++ src/views/AppHistory.vue
@@ -81,6 +81,12 @@ export default {
     this.loading = false
   },
   methods: {
+    createRandomColor() {
+      const r = Math.floor(Math.random() * 256)
+      const g = Math.floor(Math.random() * 256)
+      const b = Math.floor(Math.random() * 256)
+      return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`
+    },
     createChart() {
       this.renderChart({
         labels: this.getCategories.map(item => item.name),
@@ -93,23 +99,7 @@ export default {
             }
             return total
           }, 0)),
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
+          backgroundColor: this.pieDiagramColors,
         }],
       })
     },
@@ -129,6 +119,13 @@ export default {
         }))
       }
       return null
+    },
+    pieDiagramColors() {
+      const arr = []
+      for (let i = 0; i < this.getCategories.length; i += 1) {
+        arr.push(this.createRandomColor())
+      }
+      return arr
     },
   },
   data: () => ({
