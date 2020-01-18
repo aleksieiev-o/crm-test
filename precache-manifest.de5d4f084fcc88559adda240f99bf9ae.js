c4756cf2c19bf9e17b3a27fcdbdef168bf66269e
mandred <mandred1991@gmail.com>
2020-01-19 00:35:25 +0300

    deploy

---
+++ precache-manifest.de5d4f084fcc88559adda240f99bf9ae.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906a75eeb379d80e17fa",
+    "url": "/crm-test/css/app.1642b8a8.css"
+  },
+  {
+    "revision": "fbf00ee503c1bf804757",
+    "url": "/crm-test/css/chunk-12474982.aa29a590.css"
+  },
+  {
+    "revision": "60803bf4b3e5618a3ed7cb643910c5a8",
+    "url": "/crm-test/index.html"
+  },
+  {
+    "revision": "906a75eeb379d80e17fa",
+    "url": "/crm-test/js/app.1bcfb0a6.js"
+  },
+  {
+    "revision": "fbf00ee503c1bf804757",
+    "url": "/crm-test/js/chunk-12474982.fdb703a7.js"
+  },
+  {
+    "revision": "35e227fa5107c57f10ab",
+    "url": "/crm-test/js/chunk-1b56e541.5e11961d.js"
+  },
+  {
+    "revision": "aae175943b8b613a550f",
+    "url": "/crm-test/js/chunk-2d0b8e71.2ca9014d.js"
+  },
+  {
+    "revision": "31967de9dd305ef2392f",
+    "url": "/crm-test/js/chunk-2d0ceef0.c6f87537.js"
+  },
+  {
+    "revision": "e7f58ede9ea438594c04",
+    "url": "/crm-test/js/chunk-3d23a16d.5053a0ae.js"
+  },
+  {
+    "revision": "a5a67314a37259289aeb",
+    "url": "/crm-test/js/chunk-7d163958.674832ff.js"
+  },
+  {
+    "revision": "d53c3fab391c11858399",
+    "url": "/crm-test/js/chunk-8213ee86.75f43f63.js"
+  },
+  {
+    "revision": "963319b71221b59b9307",
+    "url": "/crm-test/js/chunk-d0bfebb6.805bff09.js"
+  },
+  {
+    "revision": "eddb0ad3b4afe19f3b87",
+    "url": "/crm-test/js/chunk-d631acf4.9f6ba52c.js"
+  },
+  {
+    "revision": "221b856325cb5c3c93a3",
+    "url": "/crm-test/js/chunk-vendors.b324803c.js"
+  },
+  {
+    "revision": "631d8ebfa0040ceea1152247ce7142b6",
+    "url": "/crm-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crm-test/robots.txt"
+  }
+]);
